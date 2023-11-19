f27c85d833355253aa2027157373ec5172d4e19f
Andrei Kryvaruchka <andrei.kryvaruchka@gmail.com>
2023-11-19 22:43:56 +0100

    Changed dependencies to static versions to avoid conflicts with newer versions of torch/xformers

--- docker/prep/launch_prep.sh
+++ docker/prep/launch_prep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source $SD_PROJECT/venv/bin/activate
 
-python3.11 -m pip install wheel triton torch>=2.1.0 torchvision>=0.16 xformers>=0.0.22.post4
+python3.11 -m pip install wheel triton torch==2.1.0 torchvision==0.16 xformers==0.0.22.post7
 mv launch_prep.py $SD_PROJECT/launch_prep.py
 
 cd $SD_PROJECT
